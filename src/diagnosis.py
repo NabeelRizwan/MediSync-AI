6f6e34b300d19c1b1495f89ba4361ccbae36560e
NabeelRizwan <150427744+NabeelRizwan@users.noreply.github.com>
2026-04-21 10:44:50 +0530

    Delete src/diagnosis.py

--- src/diagnosis.py
+++
@@ -1,8 +0,0 @@
-def predict_disease(symptoms):
-    # Placeholder logic (replace with ML model later)
-    if "fever" in symptoms:
-        return "Possible Infection"
-    elif "cough" in symptoms:
-        return "Respiratory Issue"
-    else:
-        return "Consult Doctor"
