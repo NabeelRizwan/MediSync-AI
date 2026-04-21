3742588614753b83137272cffa3696ebab0f37ff
NabeelRizwan <150427744+NabeelRizwan@users.noreply.github.com>
2026-04-21 10:50:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ from google import genai
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
+if not GOOGLE_API_KEY:
+    raise ValueError("❌ GOOGLE_API_KEY not set")
+
 client = genai.Client(api_key=GOOGLE_API_KEY)
